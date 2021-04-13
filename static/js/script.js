33f9a2109584beaf790a00681d914803250870c7
anthonrodgrs01 <anthonrodgrs01@gmail.com>
2021-04-13 14:13:22 +0530

    two player blackjack has been setup need to setup bot player

--- static/js/script.js
+++ static/js/script.js
@@ -270,6 +270,11 @@ let blackJackGame = {
     'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box','score': 0},
     'cards' : ['2','3','4','5','6','7','8','9','10','A','J','K','Q'],
     'cardsMap' : {'2': 2,'3' : 3,'4' : 4,'5' : 5,'6' : 6,'7':7,'8':8,'9':9,'10':10,'A':[1,11],'J':10,'K':10,'Q':10},
+    'wins':0,
+    'losses':0,
+    'draws':0,
+    'isStand' : false,
+    'turnsOver': false,
 };
 
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
@@ -285,12 +290,14 @@ const lossSound = new Audio('static/sounds/aww.mp3');
 
 
 function blackjackHit(){
-    let card = randomCard();
-    let activePlayer = YOU;
-    showCard(card,activePlayer);
-    updateScore(card, activePlayer);
-    console.log(activePlayer['score']);
-    showScore(activePlayer);  
+    if(blackJackGame['isStand'] === false){
+        let card = randomCard();
+        let activePlayer = YOU;
+        showCard(card,activePlayer);
+        updateScore(card, activePlayer);
+        showScore(activePlayer);
+    }
+      
 }
 
 function randomCard(){
@@ -313,7 +320,9 @@ function showCard(card , activePlayer){
 }
 
 function blackJackDeal(){
-    showResult(computeWinner());
+    if(blackJackGame['turnsOver'] === true){
+
+    blackJackGame['isStand'] = false;
     let yourImages = document.querySelector(YOU['div']).querySelectorAll('img');
     console.log(yourImages);
     for(let i=0;i<yourImages.length;i++){
@@ -327,8 +336,15 @@ function blackJackDeal(){
     document.querySelector(DEALER['scoreSpan']).textContent = 0;
     document.querySelector(YOU['scoreSpan']).style.color = 'white';
     document.querySelector(DEALER['scoreSpan']).style.color = 'white';
+    document.querySelector('#blackjack-result').textContent = "Let's Play";
+    document.querySelector('#blackjack-result').style.color = 'black';
     YOU['score'] = 0;
     DEALER['score'] = 0;
+    blackJackGame['turnsOver'] = false;
+    
+    
+    }
+    
     
     
     
@@ -363,10 +379,17 @@ function showScore(activePlayer){
 }
 
 function dealerLogic(){
+    blackJackGame['isStand'] = true;
     card = randomCard();
     showCard(card,DEALER);
     updateScore(card, DEALER);
     showScore(DEALER);
+
+    if(DEALER['score'] > 15){
+        blackJackGame['turnsOver'] = true;
+        let winner = computeWinner();
+        showResult(winner);
+    }
 }
 
 function computeWinner(){
@@ -374,37 +397,41 @@ function computeWinner(){
     if(YOU['score'] <=21){
         if(YOU['score'] > DEALER['score'] || DEALER['score'] > 21){
             winner = YOU;
-            console.log('You Won!');
+            blackJackGame['wins']++;
         }else if(YOU['score'] < DEALER['score']){
             winner = DEALER;
-            console.log('You lost!');
+            blackJackGame['losses']++;
         }else if(YOU['score'] === DEALER['score']){
-            console.log('You Tied!')
+            blackJackGame['draws']++;
         }
     }else if(YOU['score'] > 21 && DEALER['score'] <= 21){
         winner = DEALER;
-        console.log('You Lost!');
+        blackJackGame['losses']++;
     }else if(YOU['score'] > 21 && DEALER['score'] > 21){
-        console.log('You Tied');
+        blackJackGame['draws']++;
     }
 
-    console.log('Winner is' + winner);
+    console.log(blackJackGame);
     return winner;
 
 }
 
 function showResult(winner){
-    let message;
+    if(blackJackGame['turnsOver'] === true){
+        let message;
     let messageColor;
     if(winner === YOU){
+        document.querySelector('#wins').textContent = blackJackGame['wins'];
         message = 'You Won!';
         messageColor = 'green';
         winSound.play();
     }else if(winner === DEALER){
+        document.querySelector('#losses').textContent = blackJackGame['losses'];
         message = 'You Lost!';
         messageColor = 'red';
         lossSound.play();
     }else{
+        document.querySelector('#draws').textContent = blackJackGame['draws'];
         message = 'You tied!';
         messageColor = 'Yellow';
     }
@@ -412,6 +439,8 @@ function showResult(winner){
     document.querySelector('#blackjack-result').textContent = message;
     document.querySelector('#blackjack-result').style.color = messageColor;
 
+    }
+    
 }
 
 
